0685a378452c4c075abdef3572aa952f9e3c800d
RyanK1NY <ryank1ny@gmail.com>
2017-05-01 20:44:25 -0400

    Got API Key from Google, begin work on file

--- gmaps.py
+++ gmaps.py
@@ -1,3 +1,16 @@
 # handle map functionality
 
 import googlemaps
+import json
+
+# Imports credentials from maps_keys.txt
+api_keys = {}
+with open('maps_keys.txt', 'r') as f:
+    for line in f:
+        line_split = line.split()
+        api_keys[line_split[0]] = str(line_split[1])
+
+gmaps_key = api_keys['API_KEY']
+
+gmaps = googlemaps.Client(key = gmaps_key)
+
