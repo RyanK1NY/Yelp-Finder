b0048e73d84c52de87b85b83c68dddbbfe5bfccd
RyanK1NY <ryank1ny@gmail.com>
2017-05-27 15:48:47 -0400

    Added main and __name__ main logic. get_business_info, search_business now take token as an argument. Categories dict no longer global, dumped into file in get_business_info. update_categories now takes categories as an argument.

--- yelp_fusion.py
+++ yelp_fusion.py
@@ -1,12 +1,6 @@
 '''
 Using Yelp Fusion API
-'''
 
-import json
-import requests
-from rauth import OAuth2Service
-
-'''
 Workflow:
 - access_token.txt contains token
 - if token from file not valid, retrieve a token
@@ -26,6 +20,10 @@ yelp = OAuth2Service(
     access_token_url = 'https://api.yelp.com/oauth2/token'
 )
 '''
+
+import json
+import requests
+from rauth import OAuth2Service
 # Opens file and populates credentials dict
 def get_credentials():
     api_keys = {}
@@ -66,7 +64,7 @@ Updates global categories dict with aliases, titles, and business ids.
 - id is business id for businesses that belong to category, businesses can be in multiple categories.
 returns categories dict
 '''
-def update_categories(dict_input):
+def update_categories(dict_input, categories):
     assert type(dict_input) is dict
     assert 'aliases' in dict_input
     assert 'titles' in dict_input
@@ -100,42 +98,8 @@ def get_businesses_from_file(business_list):
                 restaurant_names.append(line)
         return restaurant_names
 
-# Takes a list of business names, adds them to business_list(To do) and sends info on category to update_categories to update that dict
-# todo: account for no results being found
-def get_business_info(business_names):
-    assert type(business_names) is list
-    business_info = {}
-    try:
-        for item in business_names:
-            business = search_business(item)
-            # only enter into new_entry if not closed
-            # check if entry exists, if not put into "cannot find" file
-            # Only process if business is still open
-            business = business['businesses'][0]
-            if business['is_closed'] is False:
-                business_categories = business['categories']
-                aliases = []
-                titles = []
-
-                for sub_category in business_categories:
-                    aliases.append(sub_category['alias'])
-                    titles.append(sub_category['title'])
-
-                business_entry = {
-                    'id': business['id'],
-                    'name': business['name'],
-                    'location': ' '.join(business['location']['display_address']),
-                    'categories': aliases
-                }
-                update_categories({'aliases': aliases, 'titles': titles, 'id': business['id']})
-                print(business_entry)
-    except AssertionError:
-        print("Invalid: Argument for get_business_info must be list")
-    return business_names
-
-
 # Searches business by id
-def search_business(business_name):
+def search_business(business_name, token):
     search_url = 'https://api.yelp.com/v3/businesses/search?location=nyc&term='+business_name+'&limit=1'
     headers = {
         'Authorization': 'Bearer %s' % token
@@ -143,24 +107,62 @@ def search_business(business_name):
     r = requests.get(search_url, headers=headers)
     return r.json()
 
-restaurant_file = "restaurants.txt"
-credentials = get_credentials()
-token = credentials['access_token']
+# Takes a list of business names, adds them to business_list(To do) and sends info on category to update_categories to update that dict
+# todo: account for no results being found
+def get_business_info(business_names, token):
+    assert type(business_names) is list
+    business_info = {}
+    categories = {}
+    try:
+        for item in business_names:
+            business = search_business(item, token)
+            # only enter into new_entry if not closed
+            # check if entry exists, if not put into "cannot find" file
+            # Only process if business is still open
+            if business['total'] == 0:
+                print(item+" not found.")
+            else:
+                business = business['businesses'][0]
+                print(business['id'])
+                if business['is_closed'] is False:
+                    business_categories = business['categories']
+                    aliases = []
+                    titles = []
 
-categories = {}
-'''
-businesses = get_businesses_from_file(restaurant_file)
-business_info = get_business_info(businesses)
-business_file = open('output_test.json', 'w')
-json.dump(businesses, business_info)
-business_file.close()
+                    for sub_category in business_categories:
+                        aliases.append(sub_category['alias'])
+                        titles.append(sub_category['title'])
 
-categories_file = open('categories_output.json', 'w')
-json.dump(categories, categories_file)
-categories_file.close()
+                    business_entry = {
+                        'id': business['id'],
+                        'name': business['name'],
+                        'location': ' '.join(business['location']['display_address']),
+                        'categories': aliases
+                    }
+                    categories = update_categories({'aliases': aliases, 'titles': titles, 'id': business['id']}, categories)
+                    business_info[business['id']] = business_entry
+    except AssertionError:
+        print("Invalid: Argument for get_business_info must be list")
+    # Puts categories into file
+    categories_file = open('categories_output.json', 'w')
+    json.dump(categories, categories_file)
+    categories_file.close()
+    return business_info
 
-print(categories)
-print("Done")
-'''
-print(search_business("swelt generation"))
-print(search_business("hotel chantel"))
+def main():
+    restaurant_file = "restaurants.txt"
+    credentials = get_credentials()
+    token = credentials['access_token']
+    businesses = get_businesses_from_file(restaurant_file)
+    business_info = get_business_info(businesses, token)
+
+    business_file = open('output_test.json', 'w')
+    json.dump(business_info, business_file)
+    business_file.close()
+
+if __name__ == '__main__':
+    main()
+
+
+# print(search_business("swelt generation"))
+# print(search_business("hotel chantel")
